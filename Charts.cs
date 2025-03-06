7cbde7266fb0ac68f857b345b0eafb4732c2cd14
xnuke2 <xmnikit@gmail.com>
2025-03-07 02:54:01 +0300

    переделан показ графиков

--- Charts.cs
+++ Charts.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
 
 namespace mslab2
 {
@@ -23,64 +24,115 @@ namespace mslab2
 
         private void Charts_Load(object sender, EventArgs e)
         {
-            listBox1.Items.Clear();
-
-            for (int j = 1; j < x[0][0].Length; j++)
+            int loc_x = 5;
+            int loc_y = 5;
+            for (int i = 0; i < x.Length; i++)
             {
-                listBox1.Items.Add("X" + j);
+                loc_x = 5;
+                for (int j = 1; j < x[i][0].Length; j++)
+                {
+                    Chart chart1 = new Chart();
+                    chart1.Series.Add("X"+j);
+                    chart1.Size = new Size(300, 300);
+                    chart1.Location = new Point(loc_x, loc_y);
+                    chart1.ChartAreas.Add(new ChartArea(""));
+                    chart1.Legends.Add("");
+                    chart1.Series[0].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+                    chart1.Titles.Add("X" + j + " при h = " + pg[i][0]);
+                    switch (j)
+                    {
+                        case 1:
+                            chart1.Series[0].Color = Color.Blue;
+                            break;
+                        case 2:
+                            chart1.Series[0].Color = Color.Red;
+                            break;
+                        case 3:
+                            chart1.Series[0].Color = Color.Orange;
+                            break;
+                        case 4:
+                            chart1.Series[0].Color = Color.Green;
+                            break;
+                        case 5:
+                            chart1.Series[0].Color = Color.Brown;
+                            break;
+                    }
+                    
+                    for (int k = 0; k < x[i].Length; k++)
+                    {
+                        chart1.Series[0].Points.AddXY(x[i][k][0]- pg[i][0], x[i][k][j]);
+                    }
+                    Controls.Add(chart1);
+                    chart1.Visible = true;
+                    loc_x += 305;
+                }
+                loc_y += 305;
             }
-            listBox1.SelectedIndex = 0;
-            listBox2.Items.Clear();
-
-            for (int j = 0; j < pg.Length; j++)
-            {
-                listBox2.Items.Add(pg[j][0]);
-            }
-            listBox2.SelectedIndex = 0;
-
+            loc_x = 5;
+            Chart chart2 = new Chart();
             chart2.Series.Add("q");
-            chart2.Series[0].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            chart2.Size = new Size(300, 300);
+            chart2.Location = new Point(loc_x, loc_y);
+            chart2.ChartAreas.Add(new ChartArea(""));
+            chart2.Legends.Add("");
             chart2.Series[0].IsXValueIndexed = true;
+            chart2.Series[0].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Column;
+            chart2.Titles.Add("Относительная погрешность по сравнеию с преведущим щагом");
+            loc_x += 305;
+            Chart chart3 = new Chart();
             chart3.Series.Add("t");
+            chart3.Size = new Size(300, 300);
+            chart3.Location = new Point(loc_x, loc_y);
+            chart3.ChartAreas.Add(new ChartArea(""));
+            chart3.Legends.Add("");
             chart3.Series[0].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
             chart3.Series[0].IsXValueIndexed = true;
+            chart3.Titles.Add("Время выполнения(в тиках) в зависимость от шага");
             chart3.Series[0].Points.AddXY(pg[0][0], pg[0][2]);
             for (int j = 1; j < pg.Length; j++)
             {
                 chart2.Series[0].Points.AddXY(pg[j][0], pg[j][1]);
                 chart3.Series[0].Points.AddXY(pg[j][0], pg[j][2]);
             }
+            Controls.Add(chart2);
+            chart2.Visible = true;
+            Controls.Add(chart3);
+            chart3.Visible = true;
+
+
+
+
 
 
         }
 
-        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
-        {
-            chart1.Series.Clear();
-            int i = listBox1.SelectedIndex;
-            if (listBox2.SelectedIndex == -1) return;
-            int k = listBox2.SelectedIndex;
-            chart1.Series.Add("X" + (i + 1));
-            chart1.Series[0].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
-            for (int j = 1; j < x[k].Length; j++)
-            {
-                chart1.Series[0].Points.AddXY(x[k][j][0], x[k][j][i+1]);
-            }
-        }
+        //private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        //{
+        //    chart1.Series.Clear();
+        //    int i = listBox1.SelectedIndex;
+        //    if (listBox2.SelectedIndex == -1) return;
+        //    int k = listBox2.SelectedIndex;
+        //    chart1.Series.Add("X" + (i + 1));
+        //    chart1.Series[0].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+        //    for (int j = 1; j < x[k].Length; j++)
+        //    {
+        //        chart1.Series[0].Points.AddXY(x[k][j][0], x[k][j][i+1]);
+        //    }
+        //}
 
-        private void listBox2_SelectedIndexChanged(object sender, EventArgs e)
-        {
-            chart1.Series.Clear();
-            if (listBox1.SelectedIndex == -1) return;
-            int i = listBox1.SelectedIndex;
-            int k = listBox2.SelectedIndex;
-            chart1.Series.Add("X" + (i + 1));
-            chart1.Series[0].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
-            for (int j = 1; j < x[k].Length; j++)
-            {
-                chart1.Series[0].Points.AddXY(x[k][j][0], x[k][j][i + 1]);
-            }
-        }
+        //private void listBox2_SelectedIndexChanged(object sender, EventArgs e)
+        //{
+        //    chart1.Series.Clear();
+        //    if (listBox1.SelectedIndex == -1) return;
+        //    int i = listBox1.SelectedIndex;
+        //    int k = listBox2.SelectedIndex;
+        //    chart1.Series.Add("X" + (i + 1));
+        //    chart1.Series[0].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+        //    for (int j = 1; j < x[k].Length; j++)
+        //    {
+        //        chart1.Series[0].Points.AddXY(x[k][j][0], x[k][j][i + 1]);
+        //    }
+        //}
     }
 }
 
--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -36,8 +36,9 @@
             // 
             // numericUpDown1
             // 
-            this.numericUpDown1.DecimalPlaces = 2;
-            this.numericUpDown1.Location = new System.Drawing.Point(50, 12);
+            this.numericUpDown1.DecimalPlaces = 10;
+            this.numericUpDown1.Location = new System.Drawing.Point(38, 10);
+            this.numericUpDown1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.numericUpDown1.Maximum = new decimal(new int[] {
             999,
             0,
@@ -49,7 +50,7 @@
             0,
             131072});
             this.numericUpDown1.Name = "numericUpDown1";
-            this.numericUpDown1.Size = new System.Drawing.Size(160, 22);
+            this.numericUpDown1.Size = new System.Drawing.Size(153, 20);
             this.numericUpDown1.TabIndex = 0;
             this.numericUpDown1.Value = new decimal(new int[] {
             1,
@@ -60,17 +61,19 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 14);
+            this.label1.Location = new System.Drawing.Point(9, 11);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(32, 16);
+            this.label1.Size = new System.Drawing.Size(27, 13);
             this.label1.TabIndex = 1;
             this.label1.Text = "Шаг";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(12, 40);
+            this.button1.Location = new System.Drawing.Point(9, 32);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(198, 26);
+            this.button1.Size = new System.Drawing.Size(182, 21);
             this.button1.TabIndex = 3;
             this.button1.Text = "Расчитать";
             this.button1.UseVisualStyleBackColor = true;
@@ -78,14 +81,18 @@
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(327, 73);
+            this.ClientSize = new System.Drawing.Size(202, 59);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.numericUpDown1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.MaximumSize = new System.Drawing.Size(218, 98);
+            this.MinimumSize = new System.Drawing.Size(218, 98);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
